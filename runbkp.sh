c2811e38c1a3808678d937dbecb12fc76200b6f2
mohloko <mohloko@gmail.com>
2024-03-27 11:24:37 -0300

    Update runbkp.sh
    full bkp rsync

--- runbkp.sh
+++ runbkp.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+# V1.0
+
 TODAY=`date +"%d-%b-%y"`
 
 # FULL PATH WITH BAR AT END
-SOURCEDIR="/home/cleber/testedir/"
-DESTDIR="/home/cleber/"
+BKPNAME="PROFILE"
+SOURCEDIR="/dados/samba/"
+DESTDIR="/backup/bkp/"
+LOGFILE="/var/log/bkpprofile.log"
 
 # BKP FUNCTION
 runbkp()
 {
-  echo "Backup started at `date`" >> /var/log/bkp.log
+  echo "Backup started at `date`" >> $LOGFILE
 
-  rsync -avz $SOURCEDIR $DESTDIR"$TODAY"
+  rsync -avz $SOURCEDIR $DESTDIR$BKPNAME"$TODAY" 2>> $LOGFILE
 
   if [ $? -eq 0 ]
   then
-    echo "Backup finished at `date`" >> /var/log/bkp.log
+    echo "Backup -$BKPNAME finished at `date`" >> $LOGFILE
   else
-    echo "Backup ERROR at `date`" >> /var/log/bkp.log
+    echo "^^^ Backup -$BKPNAME ERROR at `date`" >> $LOGFILE
   fi
 }
 
@@ -26,15 +30,15 @@ clearbkp()
 {
   # COUNT BY DAY
   APAGA=`date -d "-4 day" +"%d-%b-%y"`
-  RMDIR=/backup/
+  RMDIR=/backup/bkp/
 
-  rm -rf $RMDIR$APAGA
+  rm -r "$RMDIR$BKPNAME$APAGA" 2>>$LOGFILE
 
   if [ $? -eq 0 ]
   then
-    echo "Directory $RMDIR$APAGA removed at `date`" >> /var/log/bkp.log
+    echo "Directory -$RMDIR$BKPNAME$APAGA removed at `date`" >> $LOGFILE
   else
-    echo "Clear backup ERROR at `date`" >> /var/log/bkp.log
+    echo "^^^ Clear backup -$BKPNAME ERROR at `date`" >> $LOGFILE
   fi
 
 
